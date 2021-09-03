ed24f83faeeadb71d25f3c5662e83db8994c0efb
bootcat123shoecat123 <oooth16@gmail.com>
2021-09-03 19:27:35 +0800

    de

--- app/Http/Controllers/teachController.php
+++ app/Http/Controllers/teachController.php
@@ -8,10 +8,20 @@ use Illuminate\Http\Request;
 class teachController extends Controller
 {
     //
+    function updateT(Request $R){
+        $recheck=$R->validate(['infomation'=>'required','name'=>'required']);
+        teach_model::where('name',$R->Oname)->update(
+            [
+                'infomation'=>$R->infomation,
+                'name'=>$R->name
+            ]
+            );
+        return redirect('/backTeach');
+    }
     function show(){
         #report coursemap's name、url
         $cline['teach']=teach_model::all();
-        return view('idView',[
+        return view('teachView',[
             'value'=>$cline
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\idController;
 use App\Http\Controllers\topControll;
 use App\Http\Controllers\phonecontroller;
+use App\Http\Controllers\teachController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,6 +37,10 @@ Route::post('/delete/place',[topControll::class,'delete']);
 
 Route::post('/backID/Oupdate',[idController::class,'updateO']);
 Route::post('/backID/Bupdate',[idController::class,'updateB']);
+
+Route::post('/backTeach/Tupdate',[teachController::class,'updateT']);
+Route::get('/backTeach', [teachController::class,'show']);
+
 Route::get('/backID', [idController::class,'show']);
 Route::get('/phone', [phonecontroller::class,'show']);
 Route::get('/place', [topControll::class,'show']);
